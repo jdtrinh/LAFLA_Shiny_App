3ede3363285ec4a3552ddba5a1c1260a35b146d0
jdtrinh <70670697+jdtrinh@users.noreply.github.com>
2020-11-17 10:19:12 -0800

    Add files via upload

--- LAFLA Shiny Map.R
+++ LAFLA Shiny Map.R
@@ -30,39 +30,5 @@ library(tidyr)
 library(devtools)
 library(choroplethrZip)
 
-mydata <- read_xlsx("./Input/Merged LAFLA Data (Full Joins).xlsx")
-
-# Create a single flag for supervisoral distit
-mydata <- mydata %>%
-  mutate(supervisorial_district = gsub("  ", " ", str_trim(paste(ifelse(is.na(`Supervisorial District 1`), "", "1"),
-                                                                 ifelse(is.na(`Supervisorial District 2`), "", "2"),
-                                                                 ifelse(is.na(`Supervisorial District 3`), "", "3"),
-                                                                 ifelse(is.na(`Supervisorial District 4`), "", "4"),
-                                                                 ifelse(is.na(`Supervisorial District 5`), "", "5"),
-                                                                 sep=" "))),
-         # Only capitalize the first letter in each name 
-         city = str_to_title(city))
-
-# Clean Out Unusable Variables
-
-mydata <- subset(mydata, select = -c(NAME, `Zip Code.x`, `Zip Code.y`, name, zip, `Area Name`,
-                                     `Unlimited Civil (exclude Personal Injury)`,
-                                     `Unlimited Civil (Personal Injury)`,
-                                     `Family Law`, `Restraining Orders`, Probate,
-                                     `Limited Civil (exclude Collection)`,
-                                     `Limited Civil (Collection)`,
-                                     `Limited Unlawful Detainer`, `Small Claims`,
-                                     state, county, year, `Supervisorial District 1`,
-                                     `Supervisorial District 2`, `Supervisorial District 3`, 
-                                     `Supervisorial District 4`, `Supervisorial District 5`))
-names(mydata)
-
-# Create the list of variable names
-columnChoices <- colnames(mydata)
-zipChoices <- unique(mydata$GEOID)
-cityChoices <- c("All Cities", unique(mydata$city))
-supervisorChoices <- c("1", "2", "3", "4")
-congressionalChoices <- c("TBD")
-
 
 runApp(getwd())
